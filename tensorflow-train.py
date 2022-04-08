8eeb6b706a20cc1cece99c7dfb4b58bbcac73e54
Dmitry Savkin <dmitry.savkin@student.htw-berlin.de>
2022-04-08 13:19:11 +0200

    test-coco#

--- tensorflow-train.py
+++ tensorflow-train.py
@@ -80,56 +80,131 @@ for image, mask in train.take(1):
 display([sample_image, sample_mask])
 
 OUTPUT_CHANNELS = 3
+ACTIVATION = "relu"
 
-base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
-# Use the activations of these layers
-layer_names = [
-    'block_1_expand_relu',   # 64x64
-    'block_3_expand_relu',   # 32x32
-    'block_6_expand_relu',   # 16x16
-    'block_13_expand_relu',  # 8x8
-    'block_16_project',      # 4x4
-]
-layers = [base_model.get_layer(name).output for name in layer_names]
-
-# Create the feature extraction model
-down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
-down_stack.trainable = False
-
-up_stack = [
-    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
-    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
-    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
-]
-
-def unet_model(output_channels):
-  inputs = tf.keras.layers.Input(shape=[128, 128, 3])
-  x = inputs
-
-  # Downsampling through the model
-  skips = down_stack(x)
-  x = skips[-1]
-  skips = reversed(skips[:-1])
-
-  # Upsampling and establishing the skip connections
-  for up, skip in zip(up_stack, skips):
-    x = up(x)
-    concat = tf.keras.layers.Concatenate()
-    x = concat([x, skip])
-
-  # This is the last layer of the model
-  last = tf.keras.layers.Conv2DTranspose(
-      output_channels, 3, strides=2,
-      padding='same')  #64x64 -> 128x128
-
-  x = last(x)
-  return tf.keras.Model(inputs=inputs, outputs=x)
 
 def create_mask(pred_mask):
   pred_mask = tf.argmax(pred_mask, axis=-1)
   pred_mask = pred_mask[..., tf.newaxis]
   return pred_mask[0]
+
+
+def convolution_block(x, filters, size, strides=(1, 1), padding='same', activation=True):
+    x = Conv2D(filters, size, strides=strides, padding=padding)(x)
+    x = BatchNormalization()(x)
+    if activation == True:
+        x = Activation(ACTIVATION)(x)
+    return x
+
+
+def residual_block(blockInput, num_filters=16):
+    x = Activation(ACTIVATION)(blockInput)
+    x = BatchNormalization()(x)
+    x = convolution_block(x, num_filters, (3, 3))
+    x = convolution_block(x, num_filters, (3, 3), activation=False)
+    x = Add()([x, blockInput])
+    return x
+
+
+# Build model
+def build_model(input_layer, start_neurons, DropoutRatio=0.5, nn_image_size=101):
+    # 101 -> 50
+    conv1 = Conv2D(start_neurons * 1, (3, 3), activation=None, padding="same")(input_layer)
+    conv1 = residual_block(conv1, start_neurons * 1)
+    conv1 = residual_block(conv1, start_neurons * 1)
+    conv1 = Activation(ACTIVATION)(conv1)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+    pool1 = Dropout(DropoutRatio / 2)(pool1)
+
+    # 50 -> 25
+    conv2 = Conv2D(start_neurons * 2, (3, 3), activation=None, padding="same")(pool1)
+    conv2 = residual_block(conv2, start_neurons * 2)
+    conv2 = residual_block(conv2, start_neurons * 2)
+    conv2 = Activation(ACTIVATION)(conv2)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+    pool2 = Dropout(DropoutRatio)(pool2)
+
+    # 25 -> 12
+    conv3 = Conv2D(start_neurons * 4, (3, 3), activation=None, padding="same")(pool2)
+    conv3 = residual_block(conv3, start_neurons * 4)
+    conv3 = residual_block(conv3, start_neurons * 4)
+    conv3 = Activation(ACTIVATION)(conv3)
+    pool3 = MaxPooling2D((2, 2))(conv3)
+    pool3 = Dropout(DropoutRatio)(pool3)
+
+    # 12 -> 6
+    conv4 = Conv2D(start_neurons * 8, (3, 3), activation=None, padding="same")(pool3)
+    conv4 = residual_block(conv4, start_neurons * 8)
+    conv4 = residual_block(conv4, start_neurons * 8)
+    conv4 = Activation(ACTIVATION)(conv4)
+    pool4 = MaxPooling2D((2, 2))(conv4)
+    pool4 = Dropout(DropoutRatio)(pool4)
+
+    # Middle
+    convm = Conv2D(start_neurons * 16, (3, 3), activation=None, padding="same")(pool4)
+    convm = residual_block(convm, start_neurons * 16)
+    convm = residual_block(convm, start_neurons * 16)
+    convm = Activation(ACTIVATION)(convm)
+
+    # 6 -> 12
+    deconv4 = Conv2DTranspose(start_neurons * 8, (3, 3), strides=(2, 2), padding="same")(convm)
+    uconv4 = concatenate([deconv4, conv4])
+    uconv4 = Dropout(DropoutRatio)(uconv4)
+
+    uconv4 = Conv2D(start_neurons * 8, (3, 3), activation=None, padding="same")(uconv4)
+    uconv4 = residual_block(uconv4, start_neurons * 8)
+    uconv4 = residual_block(uconv4, start_neurons * 8)
+    uconv4 = Activation(ACTIVATION)(uconv4)
+
+    # 12 -> 25
+    # deconv3 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="same")(uconv4)
+    if nn_image_size == 101:
+        deconv3 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="valid")(uconv4)
+    else:
+        deconv3 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="same")(uconv4)
+    uconv3 = concatenate([deconv3, conv3])
+    uconv3 = Dropout(DropoutRatio)(uconv3)
+
+    uconv3 = Conv2D(start_neurons * 4, (3, 3), activation=None, padding="same")(uconv3)
+    uconv3 = residual_block(uconv3, start_neurons * 4)
+    uconv3 = residual_block(uconv3, start_neurons * 4)
+    uconv3 = Activation(ACTIVATION)(uconv3)
+
+    # 25 -> 50
+    deconv2 = Conv2DTranspose(start_neurons * 2, (3, 3), strides=(2, 2), padding="same")(uconv3)
+    uconv2 = concatenate([deconv2, conv2])
+
+    uconv2 = Dropout(DropoutRatio)(uconv2)
+    uconv2 = Conv2D(start_neurons * 2, (3, 3), activation=None, padding="same")(uconv2)
+    uconv2 = residual_block(uconv2, start_neurons * 2)
+    uconv2 = residual_block(uconv2, start_neurons * 2)
+    uconv2 = Activation(ACTIVATION)(uconv2)
+
+    # 50 -> 101
+    if nn_image_size == 101:
+        deconv1 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="valid")(uconv2)
+    else:
+        deconv1 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="same")(uconv2)
+    uconv1 = concatenate([deconv1, conv1])
+
+    uconv1 = Dropout(DropoutRatio)(uconv1)
+    uconv1 = Conv2D(start_neurons * 1, (3, 3), activation=None, padding="same")(uconv1)
+    uconv1 = residual_block(uconv1, start_neurons * 1)
+    uconv1 = residual_block(uconv1, start_neurons * 1)
+    uconv1 = Activation(ACTIVATION)(uconv1)
+
+    uconv1 = Dropout(DropoutRatio / 2)(uconv1)
+    output_layer = Conv2D(80, (1, 1), padding="same", activation="sigmoid")(uconv1)
+
+    return output_layer
+
+
+def CreateModel(img_size_target, channels=3):
+    input_layer = Input((img_size_target, img_size_target, channels))
+    output_layer = build_model(input_layer, 80, 0.5, nn_image_size=img_size_target)
+    model = Model(input_layer, output_layer)
+    return model
+
 
 
 
@@ -151,7 +226,8 @@ class DisplayCallback(tf.keras.callbacks.Callback):
 
 
 
-model = unet_model(OUTPUT_CHANNELS)
+model = CreateModel(img_size_target=128)
+model.summary()
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
